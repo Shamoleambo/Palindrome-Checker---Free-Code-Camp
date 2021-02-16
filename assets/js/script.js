c8046f4a16cf92e64b9c0cf395ca47ae3b7b10b6
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-16 13:35:27 -0300

    Implementacao logica basica do palindromo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,18 @@ const input = document.querySelector('input');
 const p = document.querySelector('p');
 
 input.addEventListener('keyup', e => {
-    p.innerHTML = e.target.value;
+    let string = e.target.value;
+
+    let stringArray = string
+        .replace(/\s/g, '')
+        .split('');
+    let reverseStringArray = [...stringArray].reverse();
+    let isPalindrome = false;
+
+    if(stringArray.length>=2){
+        isPalindrome = stringArray.every(letter => letter===reverseStringArray[stringArray.indexOf(letter)] ? true : false);
+    
+        console.log(isPalindrome);
+    }
+    
 });
