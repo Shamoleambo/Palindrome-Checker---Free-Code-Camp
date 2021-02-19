be6d6b795d958136b373ad6c00a6018c039531ea
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-19 14:46:42 -0300

    Separacao dos procedimentos em funcoes com diferentes responsabilidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,36 @@
 const input = document.querySelector('input');
-const p = document.querySelector('p');
+const digitadoDisplay = document.querySelector('#digitadoDisplay');
+const tagPalindromo = document.querySelector('#palindromo');
 
 input.addEventListener('keyup', e => {
     let string = e.target.value;
+    digitadoDisplay.innerHTML = string;
 
-    let stringArray = string
+    let [array, invertedArray] = arrays(string);
+    const palindromo = isPalindrome(array, invertedArray);
+
+    console.log(palindromo);
+    if(palindromo){
+        tagPalindromo.style.visibility = 'visible';
+    }
+
+});
+
+function isPalindrome(arr, inv){
+    let isPalindrome = false;
+
+    if(arr.length>=2){
+        isPalindrome = arr.every(letter => letter===inv[arr.indexOf(letter)] ? true : false);
+    }
+    
+    return isPalindrome;
+}
+
+function arrays(str){
+    let stringArray = str
         .replace(/\s/g, '')
         .split('');
     let reverseStringArray = [...stringArray].reverse();
-    let isPalindrome = false;
 
-    if(stringArray.length>=2){
-        isPalindrome = stringArray.every(letter => letter===reverseStringArray[stringArray.indexOf(letter)] ? true : false);
-    
-        console.log(isPalindrome);
-    }
-    
-});
+    return [stringArray, reverseStringArray];
+}
